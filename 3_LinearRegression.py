759d5f87b2555280588cda9386303c21e9aab6f7
ThanatPay <t.payatsuporn@gmail.com>
2024-02-06 08:04:16 +0700

    update Regression

--- 3_LinearRegression.py
+++ 3_LinearRegression.py
@@ -2,49 +2,53 @@ import numpy as np
 import math
 
 class LinearRegression:
-    def adjust_theta(self,X,y,theta,learning_rate):
-        y_pred=self.predict(X,theta)
-        gd=np.matmul(y-y_pred,X)
-        return theta+(learning_rate*gd)
+    def adjust_weight(self,X,y,theta,bias,learning_rate):
+        y_pred=self.predict(X,theta,bias)
+        d_theta=np.matmul(y-y_pred,X)
+        d_bias=np.sum(y-y_pred)
+        return theta+(learning_rate*d_theta),bias+(learning_rate*d_bias)
 
     def mean_square_error(self,y,y_pred):
         return np.mean((y-y_pred)**2)
 
-    def predict(self,X,theta):
-        return np.matmul(X,theta)
+    def predict(self,X,theta,bias):
+        return np.matmul(X,theta)+bias
         
-    def train(self, X, y, epoch, learning_rate, batch):
+    def train(self, X, y, epoch, learning_rate, batch_size):
         # Convert to NumPy arrays if not already
         X=np.array(X)
         y=np.array(y)
         # random first theta
         theta=np.random.rand(X.shape[1])
+        bias=np.random.rand(1)
         # set batch
-        X_batch=np.reshape(X,(batch,X.shape[0]//batch,X.shape[1]))
-        y_batch=np.reshape(y,(batch,y.shape[0]//batch))
+        num_batch=X.shape[0]//batch_size
+        X_batch=np.reshape(X,(num_batch,batch_size,X.shape[1]))
+        y_batch=np.reshape(y,(num_batch,batch_size))
         # for select best loss
         best_mse=math.inf
         
         for i in range(epoch):
-
-            for j in range(batch):
+            for j in range(num_batch):
                 # adjust theta
-                theta=self.adjust_theta(X_batch[j],y_batch[j],theta,learning_rate)
+                theta,bias=self.adjust_weight(X_batch[j],y_batch[j],theta,bias,learning_rate)
             # calaulate loss
-            y_pred=self.predict(X,theta) 
+            y_pred=self.predict(X,theta,bias) 
             mse=self.mean_square_error(y,y_pred)
             # select best loss
             if mse < best_mse:
                 best_mse=mse
                 best_theta=theta
+                best_bias=bias
             print("epoch",i)
             print("loss",mse)
             print("---------------")
 
-        return best_theta,best_mse
+        return best_theta,best_bias,best_mse
     
-# X=np.array([[1,1],[2,1],[3,2],[4,2]])
-# y=np.array([1,2,3,4])
-# L1=LinearRegression()
-# theta,_=L1.train(X,y,10,0.1,2)
-# print(L1.predict(X,theta))
+X=np.array([[1,1],[2,1],[3,2],[4,2]])
+y=np.array([1,2,3,4])
+L1=LinearRegression()
+theta,bias,_=L1.train(X,y,10,0.1,2)
+print(theta,bias)
+print(L1.predict(X,theta,bias))
--- 4_LogisticRegression.py
+++ 4_LogisticRegression.py
@@ -3,47 +3,62 @@ import math
 
 # logistic for binary class
 class LogisticRegression:
-    def __init__(self,data,target):
-        self.data=data
-        self.m=len(data)
-        self.target=target
-        self.theta=np.random.rand(data.shape[1])
+    def adjust_weight(self,X,y,theta,bias,learning_rate):
+        y_prob=self.probability(X,theta,bias)
+        d_theta=np.matmul(y-y_prob,X)
+        d_bias=np.sum(y-y_prob)
+        return theta+(learning_rate*d_theta),bias+(learning_rate*d_bias)
 
+    def log_likelihood_error(self,y,y_pred):
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return np.mean((-y*np.log(y_pred))-((1-y)*np.log(1-y_pred)))
+    
     def sigmoid(self,x):
         return 1 / (1 + np.exp(-np.array(x,dtype=float)))
-        
-    def adjust_theta(self,theta,learning_rate):
-        prob=self.probability(self.data,theta)
-        gd=np.matmul(self.target-prob,self.data)
-        return theta+(learning_rate*gd)
 
-    def log_likelihood_error(self,pred,target):
-        epsilon = 1e-15
-        pred = np.clip(pred, epsilon, 1 - epsilon)
-        return np.mean((-target*np.log(pred))-((1-target)*np.log(1-pred)))
-
-    def probability(self,data,theta):
-        return self.sigmoid(np.matmul(data,theta))
+    def probability(self,X,theta,bias):
+        return self.sigmoid(np.matmul(X,theta)+bias)
     
-    def prediction(self,data,theta,threshold):
-        prob=self.probability(data,theta)
-        pred=np.array(prob>=threshold,dtype=int)
-        return pred
+    def predict(self,X,theta,bias,threshold):
+        prob=self.probability(X,theta,bias)
+        return np.array(prob>=threshold,dtype=int)
         
-    def train(self, epoch, learning_rate):
-        # first predict
+    def train(self, X, y, epoch, learning_rate, batch_size):
+        # Convert to NumPy arrays if not already
+        X=np.array(X)
+        y=np.array(y)
+        # random first theta and bias
+        theta=np.random.rand(X.shape[1])
+        bias=np.random.rand(1)
+        # set batch
+        num_batch=X.shape[0]//batch_size
+        X_batch=np.reshape(X,(num_batch,batch_size,X.shape[1]))
+        y_batch=np.reshape(y,(num_batch,batch_size))
+        # for select best loss
         best_mle=math.inf
         
         for i in range(epoch):
-            # adjust theta
-            self.theta=self.adjust_theta(self.theta,learning_rate)
-            prob=self.probability(self.data,self.theta) 
-            mle=self.log_likelihood_error(self.target,prob)
+            for j in range(num_batch):
+                # adjust theta
+                theta,bias=self.adjust_weight(X_batch[j],y_batch[j],theta,bias,learning_rate)
+            # calaulate loss
+            y_pred=self.probability(X,theta,bias) 
+            mle=self.log_likelihood_error(y,y_pred)
             if mle < best_mle:
                 best_mle=mle
-                best_theta=self.theta
+                best_theta=theta
+                best_bias=bias
             print("epoch",i)
             print("loss",mle)
             print("---------------")
 
-        return best_theta,best_mle
+        return best_theta,best_bias,best_mle
+    
+X=np.array([[1,1],[0,0],[3,3],[2,1],[4,3],[4,4]])
+y=np.array([1,1,0,1,0,0])
+L2=LogisticRegression()
+theta,bias,_=L2.train(X,y,1000,0.01,6)
+print(theta,bias)
+print(L2.probability(X,theta,bias))
+print(L2.predict(X,theta,bias,0.5))
