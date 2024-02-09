6d9a68324f96ed4bd0824f1b1479ad27a6e770e5
ThanatPay <t.payatsuporn@gmail.com>
2024-02-10 01:39:04 +0700

    update simple bayer classifier

--- 5_SimpleBayesClassifier.py
+++ 5_SimpleBayesClassifier.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class SimpleBayesClassifier:
     def fit_params(self, x, y, n_bins=10):
         self.pos_params = []
         self.neg_params = []
+        # columns = x.columns
         x = np.array(x.astype(float))
         y = np.array(y.astype(float))
 
@@ -14,33 +16,36 @@ class SimpleBayesClassifier:
         self.total_samples = self.n_pos + self.n_neg
         self.prior_pos = self.n_pos / self.total_samples
         self.prior_neg = self.n_neg / self.total_samples
-        # print(self.prior_pos, self.prior_neg)
-        # print("------------------------------")
 
         for col in range(x.shape[1]):
+
             # Extract features for 'Yes' and 'No' samples
             x_features = x[:, col]
             pos_features = x[:, col][y==1.0]
             neg_features = x[:, col][y==0.0]
-             
-            x_unique=np.unique(x_features[pd.notna(x_features)])
-            # Compute histograms for 'Yes' and 'No' samples
-            if len(x_unique) < n_bins:
-                _, x_edges = np.histogram(x_features[pd.notna(x_features)], bins=len(x_unique))
-                pos_hist, pos_edges = np.histogram(pos_features[pd.notna(pos_features)], bins=x_edges)
-                neg_hist, neg_edges = np.histogram(neg_features[pd.notna(neg_features)], bins=x_edges)
 
+            # Remove NaN values
+            x_features_no_nan = x_features[~np.isnan(x_features)]
+            pos_features_no_nan = pos_features[~np.isnan(pos_features)]
+            neg_features_no_nan = neg_features[~np.isnan(neg_features)]
+
+            # Compute histograms for 'Yes' and 'No' samples
+            x_unique=np.unique(x_features_no_nan)
+            if len(x_unique) < n_bins:
+                x_hist, x_edges = np.histogram(x_features_no_nan, bins=len(x_unique))
+                pos_hist, pos_edges = np.histogram(pos_features_no_nan, bins=x_edges)
+                neg_hist, neg_edges = np.histogram(neg_features_no_nan, bins=x_edges)
             else:
-                _, x_edges = np.histogram(x_features[pd.notna(x_features)], bins=n_bins)
-                pos_hist, pos_edges = np.histogram(pos_features[pd.notna(pos_features)], bins=x_edges)
-                neg_hist, neg_edges = np.histogram(neg_features[pd.notna(neg_features)], bins=x_edges)
+                x_hist, x_edges = np.histogram(x_features_no_nan, bins=n_bins)
+                pos_hist, pos_edges = np.histogram(pos_features_no_nan, bins=x_edges)
+                neg_hist, neg_edges = np.histogram(neg_features_no_nan, bins=x_edges)
             
             # Append histogram parameters to the respective lists
             self.pos_params.append((pos_hist, pos_edges))
             self.neg_params.append((neg_hist, neg_edges))
 
         return self.pos_params, self.neg_params
-
+    
     def predict(self, x, thresh=0):
         y_pred = []
         x = np.array(x.astype(float))
@@ -54,6 +59,7 @@ class SimpleBayesClassifier:
                 pos_hist, pos_edges = self.pos_params[i]
                 neg_hist, neg_edges = self.neg_params[i]
 
+                # Adding likelihood probabilities
                 if not np.isnan(feature):
                     pos_bin_index = np.digitize(feature, pos_edges) - 1
                     neg_bin_index = np.digitize(feature, neg_edges) - 1
@@ -79,30 +85,7 @@ class SimpleBayesClassifier:
             # Assigning class label based on the maximum log probability
             pred = 1 if np.exp(log_prob_pos) > thresh and log_prob_pos > log_prob_neg else 0
             y_pred.append(pred)
-            # print(np.exp(log_prob_pos), np.exp(log_prob_neg))
-            # print(pred)
-            # print("------------------------------")
 
         return y_pred
-
-n_bins=3
-data = pd.read_csv("hr-employee-attrition-with-null.csv")
-data.loc[data["Attrition"] == "No", "Attrition"] = 0.0
-data.loc[data["Attrition"] == "Yes", "Attrition"] = 1.0
-data.loc[data["BusinessTravel"] == "Travel_Rarely", "BusinessTravel"] = 0.0
-data.loc[data["BusinessTravel"] == "Travel_Frequently", "BusinessTravel"] = 1.0
-data.loc[data["BusinessTravel"] == "Non-Travel", "BusinessTravel"] = 2.0
-data.loc[data["Department"] == "Research & Development", "Department"] = 0.0
-data.loc[data["Department"] == "Sales", "Department"] = 1.0
-data.loc[data["Department"] == "Human Resources", "Department"] = 2.0
-x_train=data.iloc[70:, 3:7]
-y_train=data.iloc[70:, 2]
-x_test=data.head(70).iloc[:, 3:7]
-y_test=data.head(70).iloc[:, 2]
-model=SimpleBayesClassifier()
-model.fit_params(x_train,y_train)
-pred=model.predict(x_test)
-print('predict:',pred)
-print('accuracy:',np.mean(pred==y_test))
 
 
